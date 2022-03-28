2a47d817e3cfdd44b57dd0e86539d1562a3381de
andrew_l <andrewlevins@gmail.com>
2022-03-28 10:06:46 -0400

    update comments

--- lab/week-8-airtable/part-one/script.js
+++ lab/week-8-airtable/part-one/script.js
@@ -11,7 +11,7 @@ fetch('https://api.airtable.com/v0/appFOm7DDpMYM90u9/albums', {
 
     console.log(data); // first, log out your data. Explore it in the browser console.
 
-    const albumsContainer = document.querySelector('.albums-container');
+    const albumsContainer = document.querySelector('.albums-container'); // tell JS about the div we added to our html file so we can put content inside it
 
     // loop over each record (row) of our Airtable data
     data.records.forEach(album => {
@@ -20,13 +20,14 @@ fetch('https://api.airtable.com/v0/appFOm7DDpMYM90u9/albums', {
       // now we add html to our albumsContainer div
       // this is where we take our data from Airtable and put it in our html
       // think of this chunk of HTML as a component template for each entry in our database
+      // note the backticks `` below. This allows us to add html + js together using ${field} in a single block of code
       albumsContainer.innerHTML += `
-      <div class="album">
-        <h5>${album.fields.release_year}</h5>  
-        <h3>${album.fields.title}</h3>
-        <h4>${album.fields.artist}</h4>
-        <img src="${album.fields.album_cover[0].thumbnails.large.url}" width='200'/>
-      </div>
+        <div class="album">
+          <h5>${album.fields.release_year}</h5>  
+          <h3>${album.fields.title}</h3>
+          <h4>${album.fields.artist}</h4>
+          <img src="${album.fields.album_cover[0].thumbnails.large.url}" width='200'/>
+        </div>
         `;
 
       // that's it!
