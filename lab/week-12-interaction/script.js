2b91ea7508005a51f690a0ee1ac8842eae1e77ee
andrew_l <andrewlevins@gmail.com>
2022-04-21 22:07:15 -0400

    week 12

--- lab/week-12-interaction/script.js
+++ lab/week-12-interaction/script.js
@@ -1,85 +1,51 @@
 let isChecked = false;
 let sliderValue = 1000;
 let instrumentType = 'all';
-let sortBy = null;
+let sortBy = 'popularity';
 
-const radio = document.querySelector('.radio-container');
 function handleSort() {
-  console.log('sort option', event.target.value);
   sortBy = event.target.value;
   generateContent();
 }
 
-const checkbox = document.querySelector('.checkbox-container');
 function handleCheckbox() {
-  console.log('checkbox checked', event.target.checked);
-  console.log(checkbox);
   isChecked = event.target.checked ? true : false;
   generateContent();
 }
 
-const dropdown = document.querySelector('.dropdown-container');
 function handleDropdown() {
-  console.log('dropdown changed to', event.target.value);
-  console.log(dropdown);
   instrumentType = event.target.value;
   generateContent();
 }
 
-const slider = document.querySelector('.range-slider-container');
+const sliderDisplayValue = document.querySelector('.range-slider-container span');
 function handleSlider() {
   sliderValue = event.target.value;
-  slider.querySelector('span').innerHTML = sliderValue;
+  sliderDisplayValue.innerHTML = `$${numberWithCommas(sliderValue)}`;
+  generateContent();
+}
+
+const reset = document.querySelector('.reset');
+const dropdown = document.querySelector('.dropdown-container select');
+const checkbox = document.querySelector('.checkbox-container input');
+const slider = document.querySelector('.range-slider-container input');
+const radio = document.querySelector('.radio-container #popularity');
+
+function resetFilters() {
+  instrumentType = 'all';
+  isChecked = false;
+  sliderValue = 1000;
+  sortBy = 'popularity';
+  sliderDisplayValue.innerHTML = sliderValue;
+  slider.value = sliderValue;
+  dropdown.value = 'all';
+  checkbox.checked = isChecked;
+  radio.checked = true;
   generateContent();
 }
 
 // content
 const content = document.querySelector('.content');
-const data = [
-  {
-    name: 'Electric Guitar',
-    type: 'string',
-    popularityRanking: 1,
-    price: 305,
-    isInStock: true,
-  },
-  {
-    name: 'Drum Kit',
-    type: 'percussion',
-    popularityRanking: 3,
-    price: 700,
-    isInStock: false,
-  },
-  {
-    name: 'Bass Guitar',
-    type: 'string',
-    popularityRanking: 2,
-    price: 130,
-    isInStock: true,
-  },
-  {
-    name: 'Cello',
-    type: 'string',
-    popularityRanking: 6,
-    price: 245,
-    isInStock: true,
-  },
-  {
-    name: 'Trumpet',
-    type: 'brass',
-    popularityRanking: 11,
-    price: 130,
-    isInStock: true,
-  },
-  {
-    name: 'Trombone',
-    type: 'brass',
-    popularityRanking: 9,
-    price: 650,
-    isInStock: false,
-  },
-];
-
 function generateContent() {
   content.innerHTML = '';
   data
@@ -96,10 +62,10 @@ function generateContent() {
     .forEach(card => {
       content.innerHTML += `
       <div class="card">
-       <p>Popularity Ranking: ${card.popularityRanking}</p>
+       <p>Popularity Ranking: ${card.popularity}</p>
         <h4>${card.name}</h4>
         <p>${card.type}</p>
-        <p>$${card.price}</p>
+        <p class="price">$${card.price}</p>
         <p class="${card.isInStock ? 'in-stock' : 'out-of-stock'}">${
         card.isInStock ? 'In stock, order today!' : 'Out of stock'
       }</p>
@@ -108,3 +74,8 @@ function generateContent() {
     });
 }
 generateContent();
+
+// utility
+function numberWithCommas(num) {
+  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
